08763b4029fc812b981b96f4dae07f6a7ecef950
Muntazir FX <b8wz2gn2kt@privaterelay.appleid.com>
2026-09-10 22:17:12 +0500

    Refactor MetalCallbacks by removing unused code
    Removed unused global variable g_mtkView and commented out print statements for command types in aether_metal_submit_swift.

--- ios/AetherApp/MetalCallbacks.swift
+++ ios/AetherApp/MetalCallbacks.swift
@@ -6,11 +6,8 @@ import Foundation
 import Metal
 import MetalKit
 
-private weak var g_mtkView: MTKView?
-
 @_cdecl("aether_metal_init_swift")
 public func aether_metal_init_swift(_ user: UnsafeMutableRawPointer?, _ w: UInt32, _ h: UInt32) -> Int32 {
-    g_mtkView = user?.assumingMemoryBound(to: MTKView.self).pointee
     print("[MetalCallbacks] init \(w)x\(h)")
     return 0  // AETHER_OK
 }
@@ -24,15 +21,15 @@ public func aether_metal_resize_swift(_ user: UnsafeMutableRawPointer?, _ w: UIn
 @_cdecl("aether_metal_submit_swift")
 public func aether_metal_submit_swift(_ user: UnsafeMutableRawPointer?, _ cmd: UnsafeRawPointer?) -> Int32 {
     guard let cmd = cmd else { return 0 }
-    // Read command type (first 4 bytes)
+    // Read command type (first 4 bytes = aether_render_cmd_type_t enum)
     let type = cmd.assumingMemoryBound(to: UInt32.self).pointee
     switch type {
-    case 1:  /* BEGIN_FRAME */  print("[MetalCallbacks] begin_frame")
-    case 2:  /* CLEAR */        print("[MetalCallbacks] clear")
-    case 3:  /* SET_VIEWPORT */ print("[MetalCallbacks] set_viewport")
-    case 4:  /* DRAW_WORLD */   print("[MetalCallbacks] draw_world")
-    case 5:  /* DRAW_HUD */     print("[MetalCallbacks] draw_hud")
-    case 6:  /* END_FRAME */    print("[MetalCallbacks] end_frame")
+    case 1:  /* BEGIN_FRAME */  break
+    case 2:  /* CLEAR */        break
+    case 3:  /* SET_VIEWPORT */ break
+    case 4:  /* DRAW_WORLD */   break
+    case 5:  /* DRAW_HUD */     break
+    case 6:  /* END_FRAME */    break
     default: break
     }
     return 0  // AETHER_OK
